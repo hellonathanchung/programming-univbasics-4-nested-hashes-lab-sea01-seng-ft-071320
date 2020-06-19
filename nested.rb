51794027f6b158082face35648c8b78e989a6c44
Nathan Chung <chung.nathanh@gmail.com>
2020-06-19 15:26:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    dennis_ritchies_language = programmer_hash[:known_for][:languages]
+    dennis_ritchies_language = programmer_hash[:known_for][:languages][0]
 end
 
 def adding_matz
