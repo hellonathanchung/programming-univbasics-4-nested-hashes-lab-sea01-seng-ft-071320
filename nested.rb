18e7d951127070af1ddf62d1b127ca1585934e55
Nathan Chung <chung.nathanh@gmail.com>
2020-06-19 15:51:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-adding_to_dennis = programmer_hash[:dennnis_to_ritchie][:languages] << "Assembly"
+adding_to_dennis = programmer_hash[:dennnis_ritchie][:languages] << "Assembly"
 return programmer_hash
 end
